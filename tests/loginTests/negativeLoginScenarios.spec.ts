fc6ac8d5dc6ffb8482bcf26c805dcdde874a030b
NaomiGilerenter7519 <nb0534187519@gmail.com>
2026-07-01 14:02:01 +0300

    Add CI-safe credential fallbacks for login scenarios

--- tests/loginTests/negativeLoginScenarios.spec.ts
+++ tests/loginTests/negativeLoginScenarios.spec.ts
@@ -2,16 +2,19 @@ import { test } from '@playwright/test';
 import LoginPage from '../../pages/LoginPage';
 import { ErrorMessages } from "../../helpers/ErrorMessages";
 import ApplicationURL from '../../helpers/ApplicationURL';
+import UserCredentials from '../../helpers/UserCredentials';
 
 test.describe('Negative Login Scenarios', () => {
     let loginPage: LoginPage;
+    const lockedOutUser = process.env.LOCKED_OUT_USER || UserCredentials.LOCKED_OUT_USER;
+    const standardUser = process.env.STANDARD_USER || UserCredentials.STANDARD_USER;
    
     test.beforeEach(async ({ page }) => {
       loginPage = new LoginPage(page);
     })
     
     test('Login with locked_out_user', async () => {
-     await loginPage.loginToApplication(process.env.LOCKED_OUT_USER);
+     await loginPage.loginToApplication(lockedOutUser);
         await loginPage.validateErrorMessage(ErrorMessages.LOGIN_WITH_LOCKED_USER);
         await loginPage.validatePageUrl(ApplicationURL.BASE_URL)
     })
@@ -22,7 +25,7 @@ test.describe('Negative Login Scenarios', () => {
     })
 
     test('Login with incorrect password', async () => {
-        await loginPage.loginToApplication(process.env.STANDARD_USER, "wrong_password");
+        await loginPage.loginToApplication(standardUser, "wrong_password");
         await loginPage.validateErrorMessage(ErrorMessages.LOGIN_WITH_INCORRECT_CREDENTIALS);
         await loginPage.validatePageUrl(ApplicationURL.BASE_URL)
     })
--- tests/loginTests/positiveLoginScenarios.spec.ts
+++ tests/loginTests/positiveLoginScenarios.spec.ts
@@ -3,12 +3,17 @@ import LoginPage from "../../pages/LoginPage";
 import ApplicationURL from "../../helpers/ApplicationURL";
 import ProductsPage from "../../pages/ProductsPage";
 import PageTitles from "../../helpers/PageTitles";
+import UserCredentials from "../../helpers/UserCredentials";
 
 
 test.describe("Positive Login Scenarios", () => {
     
     let loginPage: LoginPage;
     let productsPage: ProductsPage;
+    const standardUser = process.env.STANDARD_USER || UserCredentials.STANDARD_USER;
+    const problemUser = process.env.PROBLEM_USER || UserCredentials.PROBLEM_USER;
+    const performanceGlitchUser = process.env.PERFORMANCE_GLITCH_USER || UserCredentials.PERFORMANCE_GLITCH_USER;
+    const correctPassword = process.env.CORRECT_PASSWORD || UserCredentials.CORRECT_PASSWORD;
 
     test.beforeEach(async({page}) => {
         loginPage = new LoginPage(page);
@@ -20,17 +25,17 @@ test.describe("Positive Login Scenarios", () => {
     })
 
     test("Login with standard_user", async() => {
-        await loginPage.loginToApplication(process.env.STANDARD_USER, process.env.CORRECT_PASSWORD);
+        await loginPage.loginToApplication(standardUser, correctPassword);
         await productsPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL);
     })
 
     test("Login with problem_user", async() => {
-        await loginPage.loginToApplication(process.env.PROBLEM_USER);
+        await loginPage.loginToApplication(problemUser);
         await productsPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL)
     })
 
     test("Login with performance_glitch_user", async() => {
-        await loginPage.loginToApplication(process.env.PERFORMANCE_GLITCH_USER);
+        await loginPage.loginToApplication(performanceGlitchUser);
         await productsPage.validatePageUrl(ApplicationURL.INVENTORY_PAGE_URL)
     })
 })
